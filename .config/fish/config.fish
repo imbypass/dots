85ce3c2e637c71b53ba65fdfeb2b9f6bf5ccd257
imbypass <coleschaefer1@gmail.com>
2024-10-05 22:21:03 -0400

    Wallpaper changer coming soon? Also, idfk anymore

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,10 @@
-fastfetch
-set fish_greeting
+if status is-interactive
+    set fish_greeting
+    fastfetch
+end
 
 bind \e\e 'begin; set -l buf (commandline); commandline -r "sudo "$buf; end'
+bind -k nul accept-autosuggestion
 
 # Custom PATH environment
 fish_add_path /home/bypass/.spicetify
@@ -12,7 +15,7 @@ fish_add_path /home/linuxbrew/.linuxbrew/bin
 # starship init fish | source
 zoxide init fish --cmd cd | source
 # thefuck --alias | source
-pyenv init - | source
+# pyenv init - | source
 /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
 
 set -x ZSH "$HOME/.oh-my-zsh"
@@ -27,6 +30,12 @@ set -x SHELL 'fish'
 set -x EDITOR 'nvim'
 set -x VISUAL 'qview'
 set -x ANI_CLI_MODE 'dub'
+set -x FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi"
 
 # Custom miscellaneous aliases
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
@@ -38,6 +47,7 @@ alias tree="eza -l --tree --icons"
 alias l="ls"
 alias ls="eza --icons --grid"
 alias lsa="eza -la --icons --grid"
+alias grep="rg"
 alias cat="bat -p"
 alias nano="nvim"
 alias py3="python3"
@@ -56,3 +66,10 @@ alias fx_off="hyprshade off"
 alias fx_on="hyprshade on vibrance"
 alias fx="hyprshade toggle vibrance"
 
+# Custom ls/eza overrides
+alias l 'ls -alh'
+alias la 'eza -a'
+alias ll 'eza -lTF --group-directories-first --color=always --git --git-ignore --level 1'
+alias ll2 'eza -lTF --group-directories-first --color=always --git --git-ignore --level 2'
+alias ll2a 'eza -laTF --group-directories-first --color=always --git --level 2'
+alias ll3 'eza -lTF --group-directories-first --color=always --git --git-ignore --level 3'
