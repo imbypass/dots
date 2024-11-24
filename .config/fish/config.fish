977f29d7eddd2fadab46a002c664efbb57f92fab
imbypass <coleschaefer1@gmail.com>
2024-11-24 10:10:03 -0500

    Updated fish config to be cleaner.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,75 @@
+# Interactive shell settings
 if status is-interactive
     set TERM 'xterm-256color'
     set fish_greeting
     set fish_tmux_autostart true
     set fish_tmux_autoname_session true
     fish_vi_key_bindings
-    #bash -c "~/.config/scripts/pacman.sh"
 end
 
-function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
-end
-
-# Prepend sudo binds
+# Alt+Escape to prepend 'sudo'
 bind \e\e 'begin; set -l buf (commandline); commandline -r "sudo "$buf; end'
 bind -k nul accept-autosuggestion
+
+# Starship Transient Prompt (Left)
+function starship_transient_prompt_func
+  starship module character
+end
+
+# Starship Transient Prompt (Right)
+function starship_transient_rprompt_func
+  starship module time
+end
+
+# Initialize software
+zoxide init fish --cmd cd | source
+thefuck --alias | source
+/home/linuxbrew/.linuxbrew/bin/brew shellenv | source
+fzf --fish | source
+pyenv init - | source
+starship init fish | source
+
+# Enable transient prompt for Starship/Fish
+enable_transience
+
+# Dotfiles git alias
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+# Simplified update-grub command
+alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+
+# Custom system aliases
+alias tree="eza -l --tree --icons"
+alias ls="eza --icons"
+alias lsa="eza -la --icons"
+alias l 'ls'
+alias la 'ls -a'
+alias cl="/usr/bin/clear && pman"
+alias ex="exit"
+alias grep="rg"
+alias cat="bat"
+alias h="hyprctl"
+alias f="microfetch"
+alias py3="python3"
+alias v="nvim"
+alias vim="nvim"
+alias nano="nvim"
+
+# Ricing commands - Making little things less painful
+alias pman="bash -c ~/.config/scripts/pacman.sh"
+alias ghosts="bash -c ~/.config/scripts/ghosts.sh"
+alias wp="waypaper --random > /dev/null"
+
+# Custom shortcut aliases
+alias vps="ssh root@imbypass.pw"
+alias moviebox="ssh schaefer@10.0.0.196"
+alias ani="ani-cli --dub --skip"
+alias wg++="x86_64-w64-mingw32-g++"
+alias unset="set --erase"
+
+# Custom ricing aliases
+alias clock="tty-clock -t -c -b -B"
+alias icons="papirus-folders -t Papirus-Dark -C "
 
 # Custom PATH environment
 fish_add_path /home/bypass/.spicetify
@@ -26,74 +77,8 @@ fish_add_path /home/bypass/local/bin
 fish_add_path /home/bypass/.config/scripts
 fish_add_path /home/linuxbrew/.linuxbrew/bin
 
-# Starship Transient Prompt
-function starship_transient_prompt_func
-  starship module character
-end
-
-#function starship_transient_rprompt_func
-  #starship module time
-#end
-
-# Other Inits? (not exactly sure what to call these)
-starship init fish | source
-enable_transience
-zoxide init fish --cmd cd | source
-thefuck --alias | source
-/home/linuxbrew/.linuxbrew/bin/brew shellenv | source
-starship init fish | source
-fzf --fish | source
-pyenv init - | source
-
-# Custom miscellaneous aliases
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
-
-# Custom system aliases
-alias cl="/usr/bin/clear"
-alias tree="eza -l --tree --icons"
-alias l="ls"
-alias ls="eza --icons"
-alias lsa="eza -la --icons"
-alias grep="rg"
-alias h="hyprctl"
-alias cat="bat"
-alias nano="nvim"
-alias v="nvim"
-alias f="microfetch"
-alias py3="python3"
-alias vim="nvim"
-alias ff="fastfetch -c minimal"
-alias pman="bash -c ~/.config/scripts/pacman.sh"
-alias ghosts="bash -c ~/.config/scripts/ghosts.sh"
-alias wp="waypaper --random > /dev/null"
-alias cl="/usr/bin/clear && pman"
-alias ex="exit"
-
-# Custom shortcut aliases
-alias vps="ssh root@imbypass.pw"
-alias moviebox="ssh schaefer@10.0.0.196"
-alias ani="ani-cli --dub --skip"
-alias wg++="x86_64-w64-mingw32-g++"
-alias binds="nvim ~/.config/hypr/conf/keybinding.conf"
-alias unset="set --erase"
-
-# Custom ricing aliases
-alias clock="tty-clock -t -c -b -B"
-alias whereami="hostnamectl hostname"
-alias icat="kitty +kitten icat"
-alias icons="papirus-folders -t Papirus-Dark -C "
-alias tb_off="hyprpm disable hyprbars"
-alias tb_on="hyprpm enable hyprbars"
-alias fx_off="hyprshade off"
-alias fx_on="hyprshade on vibrance"
-alias fx="hyprshade toggle vibrance"
-
-# Custom ls/eza overrides
-alias l 'ls -alh'
-alias la 'eza -a'
-
 # Created by `pipx` on 2024-10-31 03:20:13
 set PATH $PATH /home/bypass/.local/bin
 set -x EDITOR 'nvim'
 set -x BAT_THEME 'Catppuccin Mocha'
+
