f7e330ad1b40613e33f88b7be017f9b74a8cb8b2
imbypass <coleschaefer1@gmail.com>
2025-02-22 01:53:15 -0500

    Waybar: Moved _bottom_ -> _top_ // Updated env script a bit. // More quickCss changes. // Cleaned up fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,8 @@
 # Interactive shell settings
 if status is-interactive
     set fish_greeting
-    set fish_tmux_autostart false
+    set fish_tmux_autostart true
     set fish_tmux_autoname_session true
-    fish_vi_key_bindings
 end
 
 # Yazi shell wrapper
@@ -20,22 +19,18 @@ end
 bind \e\e 'begin; set -l buf (commandline); commandline -r "sudo "$buf; end'
 bind -k nul accept-autosuggestion
 
-# Starship Transient Prompt (Left)
-function starship_transient_prompt_func
-    starship module character
-end
-
-# Starship Transient Prompt (Right)
-function starship_transient_rprompt_func
-    starship module time
-end
-
 # Initialize software
 zoxide init fish --cmd cd | source
 thefuck --alias | source
 /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
 fzf --fish | source
 starship init fish | source
+
+# Custom PATH environment
+fish_add_path /home/bypass/.spicetify
+fish_add_path /home/bypass/.config/scripts
+fish_add_path /home/linuxbrew/.linuxbrew/bin
+fish_add_path /home/bypass/.local/bin
 
 # Aliases - Configuration
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
@@ -53,6 +48,7 @@ alias fetch="bfetch"
 alias ff="fastfetch"
 alias grep="rg"
 alias h="hyprctl"
+alias hf="hyprfetch"
 alias hpm="hyprpm"
 alias hx="helix"
 alias icons="papirus-folders -t Papirus-Dark -C $1"
@@ -75,9 +71,3 @@ alias wg++="x86_64-w64-mingw32-g++"
 alias wp="swww img $1 > /dev/null"
 alias x="exit"
 alias z="zed"
-
-# Custom PATH environment
-fish_add_path /home/bypass/.spicetify
-fish_add_path /home/bypass/.config/scripts
-fish_add_path /home/linuxbrew/.linuxbrew/bin
-fish_add_path /home/bypass/.local/bin
