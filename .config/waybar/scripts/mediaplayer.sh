efe045c1d544d1658415bfa024cf9c4932a168f5
imbypass <coleschaefer1@gmail.com>
2025-03-22 10:40:27 -0400

    updated waybar. added now playing tooltip icon

--- .config/waybar/scripts/mediaplayer.sh
+++ .config/waybar/scripts/mediaplayer.sh
@@ -13,11 +13,11 @@ main() {
   new_stat=''
   while true
   do
-    player_status=$(playerctl --player=spotify status 2> /dev/null)
+    player_status=$(playerctl status 2> /dev/null)
     if [ "$player_status" = "Playing" ]; then
-      new_stat="  $(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+      new_stat=" $(playerctl metadata artist) - $(playerctl metadata title)"
     elif [ "$player_status" = "Paused" ]; then
-      new_stat="  $(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+      new_stat=" $(playerctl metadata artist) - $(playerctl metadata title)"
     fi
 
     if [ "$new_stat" != "$old_stat" ]; then
