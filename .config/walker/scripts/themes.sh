e2a716076cf2544c31d6b211205b08684e6e5b8f
imbypass <coleschaefer1@gmail.com>
2025-04-03 15:18:21 -0400

    Rounded mako more to match gtk4. Updated walker theme script.

--- .config/walker/scripts/themes.sh
+++ .config/walker/scripts/themes.sh
@@ -1,7 +1,9 @@
 list=$(flavours list | tr " " "\n")
 
 while themes= read -r theme; do
-  name=$(b=${theme##*/}; echo ${b%.*})
-
+  name=$(b=${theme##*/}; echo ${b%.*});
+  name=${name//-/ };
+  disp=( $name );
+  name=${disp[@]^};
   printf "label=${name};exec=flavours apply $theme\n"
 done <<< "$list"
