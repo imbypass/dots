cc9f4035798b759dbabc88c66e656dabda09de08
imbypass <coleschaefer1@gmail.com>
2025-04-03 16:50:22 -0400

    Updated walker theme script..? Updated mako look and feel.

--- .config/walker/scripts/themes.sh
+++ .config/walker/scripts/themes.sh
@@ -5,5 +5,5 @@ while themes= read -r theme; do
   name=${name//-/ };
   disp=( $name );
   name=${disp[@]^};
-  printf "label=${name};exec=flavours apply $theme\n"
+  printf "label=${name};exec=flavours apply $theme && notify-send -u low -a hyprland -t 750 \"Flavours\" \"Theme changed to:  ${name}\"\n"
 done <<< "$list"
