b5585a3a00c838bb9a10bf9b9f921d7b3822d4ff
imbypass <coleschaefer1@gmail.com>
2025-05-05 20:53:02 -0400

    Updated theme switcher to remove delay from notification.

--- .config/walker/scripts/themes.sh
+++ .config/walker/scripts/themes.sh
@@ -5,5 +5,5 @@ while themes= read -r theme; do
   name=${name//-/ };
   disp=( $name );
   name=${disp[@]^};
-  printf "label=${name};exec=flavours apply $theme && notify-send -u low -a hyprland -t 750 \"System theme updated!\" \"Theme changed:  ${name}\"\n"
+  printf "label=${name};exec=notify-send -u low -a hyprland -t 750 \"System theme updated!\" \"Theme changed to:  ${name}\" && flavours apply $theme\n"
 done <<< "$list"
