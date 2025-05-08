f7c5a16da4328d3f389d6efebb829572172425fb
imbypass <coleschaefer1@gmail.com>
2025-05-08 18:01:18 -0400

    Added fetch to startup.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,9 @@ if status is-interactive
     set fish_greeting
     set fish_tmux_autostart false
     set fish_tmux_autoname_session true
+    # fastfetch
+    # fuck a fastfetch, we got a bashfetch, bitch.
+    ~/.local/bin/fetch
 end
 
 # Alt+Escape to prepend 'sudo'
