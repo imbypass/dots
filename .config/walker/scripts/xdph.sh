509d2f4fc2903cd913a18cb3fc0ecc834649051e
imbypass <coleschaefer1@gmail.com>
2025-05-24 11:29:59 -0400

    Updated mako template a bit. Probably still needs work. Fetch updated to be a bit cleaner. Also added custom walker XDPH script courtesy of @andrij12345

--- .config/walker/scripts/xdph.sh
+++ .config/walker/scripts/xdph.sh
@@ -1,1 +1,18 @@
-walker -n --modules xdphpicker
+#!/bin/bash
+
+SELECTED=NULL
+TEMP_FILE=$XDG_RUNTIME_DIR/xdphpicker.temp
+
+CURRENT_TIMESTAMP=$(date +%s)
+FILE_TIMESTAMP=$(date -r $TEMP_FILE +%s)
+
+# save selection for 10 seconds in case when the app requests it a few times
+if ! [ -e $TEMP_FILE ] || [ $((CURRENT_TIMESTAMP - FILE_TIMESTAMP)) -gt 10 ]; then
+    SELECTED=$(walker -n --modules xdphpicker)
+
+    echo $SELECTED > $TEMP_FILE
+else
+    SELECTED=$(cat $TEMP_FILE)
+fi
+
+echo $SELECTED
