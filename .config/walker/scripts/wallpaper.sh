aa7291628c5f72a8ac2b1816bea07193443c4705
imbypass <coleschaefer1@gmail.com>
2025-08-14 19:48:47 -0400

    updated spicetify, updated walker wallpaper script animation, added nwg-menu to flavours config

--- .config/walker/scripts/wallpaper.sh
+++ .config/walker/scripts/wallpaper.sh
@@ -1,12 +1,15 @@
 list=$(find -L "/home/bypass/.local/share/wallpapers/" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \))
 
 while walls= read -r path; do
-  name=$(b=${path##*/}; echo ${b%.*})
+  name=$(
+    b=${path##*/}
+    echo ${b%.*}
+  )
 
   # now thats some grade-A fuckery right there! learn bash betr bro!
-  name=${name//_/ };
-  name=${name//-/ };
-  name=${name//   / };
+  name=${name//_/ }
+  name=${name//-/ }
+  name=${name//   / }
 
-  printf "image=$path;label=${name};exec=swww img --transition-fps 144 --transition-duration 1 -t fade $path && cp $path /home/bypass/.local/share/wallpapers/.wallpaper && /home/bypass/.local/bin/harmonyctl u colors &;\n"
-done <<< "$list"
+  printf "image=$path;label=${name};exec=swww img --transition-fps 144 --transition-duration 1 -t center $path && cp $path /home/bypass/.local/share/wallpapers/.wallpaper && /home/bypass/.local/bin/harmonyctl u colors &;\n"
+done <<<"$list"
