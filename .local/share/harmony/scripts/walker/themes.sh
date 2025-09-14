b7c27515ea334b7cbadc777dd902fa69ff6a7b2e
imbypass <coleschaefer1@gmail.com>
2025-09-14 18:35:16 -0400

    new colorscheme wip. new wallpapers.

--- .local/share/harmony/scripts/walker/themes.sh
+++ .local/share/harmony/scripts/walker/themes.sh
@@ -1,9 +1,14 @@
 list=$(flavours list | tr " " "\n")
 
 while themes= read -r theme; do
-  name=$(b=${theme##*/}; echo ${b%.*});
-  name=${name//-/ };
-  disp=( $name );
-  name=${disp[@]^};
-  printf "label=${name};exec=harmonyctl colors $theme && notify-send -u low -a hyprland -t 750 \"Harmony\" \"Theme changed to:  ${name}\" -i ~/.local/share/harmony/logo.svg\n"
+    name=$(b=${theme##*/}; echo ${b%.*});
+
+    if [ "$name" == "$(flavours current)" ]; then
+        name=" > $name < "
+    fi
+    name=${name//-/ };
+    disp=($name);
+    name=${disp[@]^};
+
+    printf "label=${name};exec=harmonyctl colors $theme && notify-send -u low -a hyprland -t 750 \"Harmony\" \"Theme changed to:  ${name}\" -i ~/.local/share/harmony/logo.svg\n"
 done <<< "$list"
