c32a64c201fa6f5b3bde8fb3735d040431bf3c16
imbypass <coleschaefer1@gmail.com>
2025-11-24 22:11:37 -0500

    created new fenrir color scheme

--- .config/walker/scripts/record_screen.sh
+++ .config/walker/scripts/record_screen.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-menu_cmd() {
-  walker -kHn --dmenu -t omarchy-default --width 285 --minheight 1 --maxheight 600
+recording_active() {
+    pgrep -x gpu-screen.* >/dev/null || pgrep -x wl-screenrec >/dev/null || pgrep -x wf-recorder >/dev/null
 }
 
-CHOSEN=$(printf "Start Recording\nStop Recording" | menu_cmd)
+menu_cmd() {
+  # walker -kHn --dmenu -t omarchy-default --width 285 --minheight 1 --maxheight 600
+  vicinae dmenu -p "Select Option"
+}
+
+if recording_active; then
+    CHOSEN=$(printf "Stop Recording\nStart Recording" | menu_cmd)
+else
+    CHOSEN=$(printf "Start Recording\nStop Recording" | menu_cmd)
+fi
+
+
 
 case "$CHOSEN" in
     "Start Recording") harmony-record start ;;
