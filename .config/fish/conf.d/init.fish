8686d19d2a2e6318acd3992679ef326092f6a8b3
imbypass <coleschaefer1@gmail.com>
2025-11-27 03:33:15 -0500

    config push

--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -5,6 +5,10 @@ fzf --fish | source
 starship init fish | source
 pyenv init - fish | source
 
+source ~/.config/omarchy/current/theme/colors.fish
+set --erase FZF_DEFAULT_OPTS
+source ~/.config/omarchy/current/theme/fzf.fish
+
 # Disable fish greeting
 set fish_greeting
 
