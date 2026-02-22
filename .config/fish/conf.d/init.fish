b5cbae2876c973dce5fb50fd2927dbef6fd3ab57
imbypass <coleschaefer1@gmail.com>
2026-02-22 16:05:27 -0500

    updated colors. pixel windows xp wallpaper.

--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -8,7 +8,10 @@ set --erase FZF_DEFAULT_OPTS
 source ~/.config/omarchy/current/theme/colors.fish
 source ~/.config/omarchy/current/theme/fzf.fish
 
+set -g fish_tmux_autostart true
+
 # Disable fish greeting
 function fish_greeting
-    fetch
+    echo "Arch Linux ($(uname -r | cut -d'-' -f1))"
+    echo "Copyright (c) $(date +%Y) Linux Corporation. All rights reserved."
 end
